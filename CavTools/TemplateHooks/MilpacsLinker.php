07d2708aba9f346f410d3c36eaaf93eac91fd0fb
Adam Jarvis <adam@jarv.ski>
2016-05-01 17:37:15 +0100

    forgot to push additions to Milpacs Linker

--- CavTools/TemplateHooks/MilpacsLinker.php
+++ CavTools/TemplateHooks/MilpacsLinker.php
@@ -7,35 +7,46 @@ class CavTools_TemplateHooks_MilpacsLinker
     if ($hookName == 'member_view_info_block')
 		{
 
-      //Get DB
-      $db = XenForo_Application::get('db');
-
-      //Get XF member ID
-      $member = $db->fetchRow('
-    SELECT user_id, username
-    FROM xf_user
-  ');
-
       //Get values from options
       $linker = XenForo_Application::get('options')->linkerBoolean;
 
-      //Declare Variables
-      $milpacsProfile = '';
-      $templateContent = '';
-      $milpacsUrl = '/rosters/profile?uniqueid=';
-
-      //Get Milpacs ID
-      $milpacsID = $db->fetchRow('
-  SELECT t1.relation_id, t1.user_id
-  FROM xf_pe_roster_user_relation t1
-  WHERE user_id = '.$member['user_id'].'
-');
-
-      $milpacsProfile = $milpacsUrl.$milpacsID['relation_id'];
-      $templateContent = '<dl><dt>Milpacs:</dt><dd><a href='.$milpacsProfile.' class="OverlayTrigger">Some Guy</a></dd></dl>';
 
       if($linker) {
-      $contents .= $templateContent;
+
+        //Get DB
+        $db = XenForo_Application::get('db');
+
+        //Get user paramater
+        $user = $hookParams['user'];
+
+        //Get XF user info
+        $member = $db->fetchrow('
+      SELECT user_id, username
+      FROM xf_user
+      WHERE user_id = '.$user.'
+    ');
+
+
+
+        //Declare Variables
+        $milpacsProfile = '';
+        $templateContent = '';
+        $milpacsUrl = '/rosters/profile?uniqueid=';
+        $username = 'user.name';
+
+        //Get Milpacs ID
+        $milpacsID = $db->fetchRow('
+    SELECT relation_id, user_id
+    FROM xf_pe_roster_user_relation
+    WHERE user_id = '.$member['user_id'].'
+  ');
+
+        if($milpacsID['relation_id'] != null) {
+          $milpacsProfile = $milpacsUrl.$milpacsID['relation_id'];
+          $templateContent = '<dl><dt>Milpac:</dt><dd><a target="_blank" href='.$milpacsProfile.' class="OverlayTrigger">'.$member['username'].'</a></dd></dl>';
+
+          $contents .= $templateContent;
+        }
       }
 		}
   }
