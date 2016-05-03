d7d8e25bdcda6faf339562ebb44770202a08edd6
Adam Jarvis <adam@jarv.ski>
2016-05-03 02:01:47 +0100

    comments + milpacs handler

--- Website/CavTools/CronJobs/SetBillets.php
+++ Website/CavTools/CronJobs/SetBillets.php
@@ -19,27 +19,26 @@ class CavTools_CronJobs_SetBillets
     ORDER BY user_id ASC
   ');
 
-    //Define Var
-
     //Renumber Array
     $members = array_values($members);
 
       foreach ($members as $member)
-      // for($i=0; $i < $members[count('user_id')]; $i++)
         {
+
 
           //Get Secondary Positions
           $milpacs = $db->fetchRow('
         SELECT CAST(secondary_position_ids AS CHAR(100))
         FROM xf_pe_roster_user_relation
-        WHERE user_id = 4
+        WHERE user_id = '.$member['user_id'].'
         AND secondary_position_ids IS NOT NULL
       ');
 
-      //Reset billetContent
-      //Deal with milpac value
       $billetContent = " ";
-      $list = $milpacs['secondary_group_ids'];
+
+      //If user has no milpacs, dont run
+      if($milpacs == !null) {
+      $list = $milpacs;
       $list = implode(',',$list);
       $list = explode(',',$list);
 
@@ -52,27 +51,28 @@ class CavTools_CronJobs_SetBillets
             WHERE position_id = '.$ID.'
           ');
 
-          //Deal with position Titles
+          //Deal with positions
           $positonTitle = $getPositonTitle;
           $positonTitle = implode(',',$positonTitle);
 
               //Concatonate all user secondary billets
               $billetContent .= $positonTitle;
-              $billetContent .= ", ";
-
-              //Set dataWriter values
-              $userId = $member['user_id'];
-              $userModel = XenForo_Model::create('XenForo_Model_User');
-              $userProfile = $userModel->getFullUserById($userId);
-              $customFields = unserialize($userProfile['custom_fields']);
-              $customFields['Billets'] = $billetContent;
-
-              //Use datawriter
-              $dw = XenForo_DataWriter::create('XenForo_DataWriter_User');
-              $dw->setExistingData($userProfile);
-              $dw->setCustomFields($customFields);
-              $dw->save();
+              $billetContent .= ",";
           }
+
+          //Define datawriter variables
+          $userId = $member['user_id'];
+          $userModel = XenForo_Model::create('XenForo_Model_User');
+          $userProfile = $userModel->getFullUserById($userId);
+          $customFields = unserialize($userProfile['custom_fields']);
+          $customFields['Billets'] = rtrim($billetContent, ",");
+
+          //Use datawriter
+          $dw = XenForo_DataWriter::create('XenForo_DataWriter_User');
+          $dw->setExistingData($userProfile);
+          $dw->setCustomFields($customFields);
+          $dw->save();
+        }
       }
     }
   }
