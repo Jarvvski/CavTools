08748508f68629fe449b14b8fe6a017d0c0e07e6
Adam Jarvis <adam@jarv.ski>
2016-05-05 16:38:00 +0100

    Added query for custom fields. More code comments

--- Website/CavTools/ControllerPublic/XmlGenerator.php
+++ Website/CavTools/ControllerPublic/XmlGenerator.php
@@ -12,7 +12,7 @@ class CavTools_ControllerPublic_XmlGenerator extends XenForo_ControllerPublic_Ab
     }
 
     //If user does not have perms, throw permission eror
-    if (!XenForo_Visitor::getInstance()->hasPermission('CavToolsGroupId', 'XmlGeneratorView'))
+    if (!XenForo_Visitor::getInstance()->hasPermission('XmlGeneratorView'))
     {
       throw $this->getNoPermissionResponseException();
     }
@@ -23,8 +23,8 @@ class CavTools_ControllerPublic_XmlGenerator extends XenForo_ControllerPublic_Ab
     //Get DB
     $db = XenForo_Application::get('db');
 
-    //Member query
-    $memberIDs = $db->fetchAll("
+    //Basic member query
+    $memberInfo = $db->fetchAll("
       SELECT user_id, username
       FROM xf_user
       ORDER BY username ASC
@@ -35,7 +35,7 @@ class CavTools_ControllerPublic_XmlGenerator extends XenForo_ControllerPublic_Ab
 
     //Create XML header
     $xml = new SimpleXMLElement('<?xml version="1.0"?><!DOCTYPE squad SYSTEM "squad.dtd">
-<?xml-stylesheet href="squad.xsl" type="text/xsl"?>');
+    <?xml-stylesheet href="squad.xsl" type="text/xsl"?>');
 
     //Begin XML body creation
     $squad = $xml->addChild("squad nick='' ");
@@ -48,15 +48,38 @@ class CavTools_ControllerPublic_XmlGenerator extends XenForo_ControllerPublic_Ab
     //Renumber Array
     $memberIDs = array_values($memberIDs);
 
-    foreach($memberIDs as $memberID) {
+    //Create a member section for each member
+    foreach($memberInfo as $member) {
+
+      //Member custom field query
+      $memberFields = $db->fetchAll('
+        SELECT field_value, field_id
+        FROM xf_user_field_value
+        WHERE user_id = '.$member['user_id'].'
+        ');
+
+      //Generate member xml section
+      //TODO
+      // - use DB values for items
+      // - maybe extrapolate nick based on username and cav rank
+      // - check if field has value, if not set value to null
 
       $member = $squad->addChild("member id='' nick='' ");
-      $member->addChild("name");
-      $member->addChild("email");
-      $member->addChild("icq");
-      $member->addChild("remark");
+      $member->addChild("name", "".);
+      $member->addChild("email", "" .$);
+      $member->addChild("icq", "");
+      $member->addChild("remark", "");
 
     }
+
+    //TODO 
+    // - check if need to output dtd, xsl aswell
+    // - set file paths as well as template output for manual checking
+    // - create cron job to automatically run when not using for checking xml
+    // - replace S1 XML: IMO bot on ADR ;)
+
+    //Set the xml created as the output for our template
+    $xmlOutput = $xml;
 
     //View Parameters
     $viewParams = array(
