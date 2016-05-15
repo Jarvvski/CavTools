af14382f68d597a179ae6c65d15498025c000b89
Adam Jarvis <adam@jarv.ski>
2016-05-15 21:57:28 +0100

    New file hierarchy

---
+++ TemplateHooks/MemberCardMilpacs.php
@@ -0,0 +1,50 @@
+<?php
+
+class CavTools_TemplateHooks_MemberCardMilpacs
+{
+  public static function infoContent($hookName, &$contents, array $hookParams, XenForo_Template_Abstract $template)
+  {
+    if ($hookName == 'member_card_stats')
+		{
+
+      //Get values from options
+      $linker = XenForo_Application::get('options')->linkerBoolean;
+
+
+      if($linker) {
+
+        //Get DB
+        $db = XenForo_Application::get('db');
+
+        //Get user paramater
+        $user = $hookParams['user'];
+
+        //Get XF user info
+        $member = $db->fetchrow('
+      SELECT user_id, username
+      FROM xf_user
+      WHERE user_id = '.$user.'
+    ');
+
+        //Declare Variables
+        $milpacsProfile = '';
+        $templateContent = '';
+        $milpacsUrl = '/rosters/profile?uniqueid=';
+        $username = 'user.name';
+
+        //Get Milpacs ID
+        $milpacsID = $db->fetchRow('
+    SELECT relation_id, user_id
+    FROM xf_pe_roster_user_relation
+    WHERE user_id = '.$member['user_id'].'
+  ');
+
+        if($milpacsID['relation_id'] != null) {
+          $milpacsProfile = $milpacsUrl.$milpacsID['relation_id'];
+          $templateContent = '<dt>Milpac:</dt> <dd><a class="Treck" href='.$milpacsProfile.'>'.$member['username'].'</a></dd>';
+          $contents .= $templateContent;
+        }
+      }
+		}
+  }
+}
