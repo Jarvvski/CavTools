89e5e854c7ca3f6e488e66c1966c5cd29024c6c0
Adam Jarvis <adam@jarv.ski>
2016-06-14 20:20:04 +0100

    More work on S3 events

--- library/CavTools/ControllerPublic/S3Events.php
+++ library/CavTools/ControllerPublic/S3Events.php
@@ -24,9 +24,9 @@ class CavTools_ControllerPublic_S3Events extends XenForo_ControllerPublic_Abstra
         $db = XenForo_Application::get('db');
 
         $eventList = $db->fetchAll("
-            SELECT id, name, date, prereq, description, instructor
+            SELECT event_id, event_title, event_date, event_prereq, event_description, event_instructor, event_hidden
             FROM xf_ct_s3_events
-             ORDER BY id ASC
+             ORDER BY event_id ASC
         ");
 
         // $eventList = $this->_getS3EventModel();
@@ -36,15 +36,17 @@ class CavTools_ControllerPublic_S3Events extends XenForo_ControllerPublic_Abstra
         {
             if (XenForo_Visitor::getInstance()->hasPermission('CavToolsGroupId', 'sendAWOLPM')) {
                 $canRemoveEvent = true;
-                if ($event['date'] >= $today['0'])
-                {
-                    $row .= "<tr><td><b>" . $event['date'] . "</b></td><td>" . $event['name'] . "</td><td>" . date('dMy', $event['date']) . "</td><td>" . $event['prereq'] . "</td><td>" . $event['instructor'] . "</td><td><input type=\"radio\" name=\"event[]\" value=" . $event['id'] . "></td></tr>" . PHP_EOL;
+                if ($event['hidden'] = false) {
+                    if ($event['date'] >= $today['0']) {
+                        $row .= "<tr><td><b>" . $event['date'] . "</b></td><td>" . $event['name'] . "</td><td>" . date('dMy', $event['date']) . "</td><td>" . $event['prereq'] . "</td><td>" . $event['instructor'] . "</td><td><input type=\"radio\" name=\"event[]\" value=" . $event['id'] . "></td></tr>" . PHP_EOL;
+                    }
                 }
             } else {
                 $canRemoveEvent = false;
-                if ($event['date'] >= $today['0'])
-                {
-                    $row .= "<tr><td><b>" . $event['date'] . "</b></td><td>" . $event['name'] . "</td><td>" . date('dMy', $event['date']) . "</td><td>" . $event['prereq'] . "</td><td>" . $event['instructor'] . "</td></tr>" . PHP_EOL;
+                if ($event['hidden'] = false) {
+                    if ($event['date'] >= $today['0']) {
+                        $row .= "<tr><td><b>" . $event['date'] . "</b></td><td>" . $event['name'] . "</td><td>" . date('dMy', $event['date']) . "</td><td>" . $event['prereq'] . "</td><td>" . $event['instructor'] . "</td></tr>" . PHP_EOL;
+                    }
                 }
             }
         }
