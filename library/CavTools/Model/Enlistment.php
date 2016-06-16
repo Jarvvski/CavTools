692f54c58463a6327b43cda91cc4aa706e2c70e8
Adam Jarvis <adam@jarv.ski>
2016-06-16 16:08:47 +0100

    update to enlistment model

--- library/CavTools/Model/Enlistment.php
+++ library/CavTools/Model/Enlistment.php
@@ -5,7 +5,7 @@ class CavTools_Model_Enlistment extends XenForo_Model {
     public function getEnlistmentById($enlistmentID)
     {
         return $this->_getDb()->fetchRow('
-            SELECT *
+            SELECT * 
             FROM xf_ct_rrd_enlistments
             WHERE enlistment_id = ?
         ', $enlistmentID);
@@ -16,8 +16,40 @@ class CavTools_Model_Enlistment extends XenForo_Model {
         return $this->fetchAllKeyed('
         SELECT *
         FROM xf_ct_rrd_enlistments
+        WHERE hidden = FALSE 
         ORDER BY enlistment_id DESC
         ', 'enlistment_id');
     }
+
+    public function getAllEnlistmentOrderByDate()
+    {
+        return $this->fetchAllKeyed('
+        SELECT *
+        FROM xf_ct_rrd_enlistments
+        ORDER BY enlistment_date DESC
+        ', 'enlistment_date');
+    }
+
+    public function getEnlistmentByLastName()
+    {
+        return $this->_getDb()->fetchRow('
+        SELECT *
+        FROM xf_ct_rrd_enlistments
+        WHERE last_name = ?
+        ', $lastName);
+    }
+    
+    public function getAllHiddenEnlistment()
+    {
+        return $this->fetchAllKeyed('
+        SELECT *
+        FROM xf_ct_rrd_enlistments
+        WHERE hidden = TRUE
+        ORDER BY enlistment_id ASC
+        ', 'enlistment_id');
+    }
+
+
+
 
 }
