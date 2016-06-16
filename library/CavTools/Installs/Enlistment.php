2fae1a17f83104011acaf76a0c95f89466a16335
Adam Jarvis <adam@jarv.ski>
2016-06-16 16:07:55 +0100

    Adding sql installer

---
+++ library/CavTools/Installs/Enlistment.php
@@ -0,0 +1,40 @@
+<?php
+class CavTools_Installs_Enlistment {
+
+    protected static $table = array(
+        'createQuery' => 'CREATE TABLE IF NOT EXISTS `xf_ct_rrd_enlistments` (             
+                `enlistment_id` INT( 10 ) NOT NULL AUTO_INCREMENT,
+                `user_id` INT( 10 ) UNSIGNED NOT NULL ,
+                `username` VARCHAR( 50 ) NOT NULL,
+                `last_name` VARCHAR( 50 ) NOT NULL,
+                `first_name` VARCHAR( 50 ) NOT NULL,
+                `age` VARCHAR( 50 ) NOT NULL,
+                `enlistment_date` VARCHAR ( 20 ) NOT NULL,
+                `steamID` VARCHAR( 50 ) NOT NULL,
+                `clan` TINYINT ( 1 ) NOT NULL,
+                `orders` TINYINT ( 1 ) NOT NULL,
+                `game` VARCHAR ( 50 ) NOT NULL,
+                `enlistment_type` INT ( 10 ) UNSIGNED NOT NULL,
+                `hidden` TINYINT ( 1 ) NOT NULL,
+                PRIMARY KEY (`simple_id`)
+                )
+            ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
+        'dropQuery' => 'DROP TABLE IF EXISTS `xf_ct_rrd_enlistments`'
+    );
+
+    // This is the function to create a table in the database so our addon will work.
+    public static function install()
+    {
+        $db = XenForo_Application::get('db');
+        $db->query(self::$table['createQuery']);
+    }
+
+    // This is the function to DELETE the table of our addon in the database.
+    public static function uninstall()
+    {
+        $db = XenForo_Application::get('db');
+        $db->query(self::$table['dropQuery']);
+    }
+}
+
+?>
