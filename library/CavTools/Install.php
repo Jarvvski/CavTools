50b0d2b67e95544ec7e264df2ef0689047ff4791
Adam Jarvis <adam@jarv.ski>
2016-06-20 17:54:40 +0100

    updated addon installer

--- library/CavTools/Install.php
+++ library/CavTools/Install.php
@@ -2,39 +2,56 @@
 class CavTools_Install {
 
     protected static $table = array(
-        'createQuery' => 'CREATE TABLE IF NOT EXISTS `xf_ct_rrd_enlistments` (             
-                `enlistment_id` INT( 10 ) NOT NULL AUTO_INCREMENT,
-                `user_id` INT( 10 ) UNSIGNED NOT NULL ,
+        'createEnlistments' => 'CREATE TABLE IF NOT EXISTS `xf_ct_rrd_enlistments` (             
+                `enlistment_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `user_id` INT( 10 ) UNSIGNED NOT NULL,
                 `recruiter` VARCHAR( 50 ),
                 `last_name` VARCHAR( 50 ) NOT NULL,
                 `first_name` VARCHAR( 50 ) NOT NULL,
-                `age` VARCHAR( 50 ) NOT NULL,
+                `age` INT( 10 ) UNSIGNED NOT NULL,
                 `timezone` VARCHAR( 10 ) NOT NULL,
-                `enlistment_date` VARCHAR ( 20 ) NOT NULL,
-                `steamID` INT( 10 ) NOT NULL,
-                `in_clan` TINYINT ( 1 ) NOT NULL,
+                `enlistment_date` BIGINT ( 20 ) NOT NULL,
+                `steamID` BIGINT ( 50 ) UNSIGNED NOT NULL,
+                `in_clan` VARCHAR ( 10 ) NOT NULL,
                 `past_clans` VARCHAR( 50 ),
-                `game` VARCHAR ( 50 ) NOT NULL,
-                `reenlistment` INT ( 10 ) UNSIGNED NOT NULL,
-                `hidden` TINYINT ( 1 ) NOT NULL,
+                `game` VARCHAR( 50 ) NOT NULL,
+                `reenlistment` TINYINT ( 1 ) NOT NULL,
+                `hidden` TINYINT( 1 ) NOT NULL,
+                `thread_id` INT ( 10 ) NOT NULL,
+                `vac_ban` TINYINT( 1 ) NOT NULL,
+                `under_age` TINYINT ( 1 ) NOT NULL,
                 PRIMARY KEY (`enlistment_id`)
                 )
             ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
-        'dropQuery' => 'DROP TABLE IF EXISTS `xf_ct_rrd_enlistments`'
+        'dropEnlistments' => 'DROP TABLE IF EXISTS `xf_ct_rrd_enlistments`',
+        
+        'createRRDLogs' => 'CREATE TABLE IF NOT EXISTS `xf_ct_rrd_logs` (             
+                `log_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `enlistment_id` INT( 10 ) UNSIGNED NOT NULL,
+                `user_id` INT ( 10 ) NOT NULL ,
+                `username` VARCHAR (50) NOT NULL ,
+                `log_date` BIGINT ( 20 ) NOT NULL,
+                `action_taken` VARCHAR (50) NOT NULL,
+                PRIMARY KEY (`log_id`)
+                )
+            ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
+        'dropRRDLogs' => 'DROP TABLE IF EXISTS `xf_ct_rrd_enlistments`'
     );
 
     // This is the function to create a table in the database so our addon will work.
     public static function install()
     {
-        $db = XenForo_Application::get('db');
-        $db->query(self::$table['createQuery']);
+        $db = XenForo_Application::getDb();
+        $db->query(self::$table['createEnlistments']);
+        $db->query(self::$table['createRRDLogs']);
     }
 
     // This is the function to DELETE the table of our addon in the database.
     public static function uninstall()
     {
-        $db = XenForo_Application::get('db');
-        $db->query(self::$table['dropQuery']);
+        $db = XenForo_Application::getDb();
+        $db->query(self::$table['dropEnlistments']);
+        $db->query(self::$table['dropRRDLogs']);
     }
 }
 
