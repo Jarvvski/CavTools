164c209b2dab13120bd653fca0bdc72cd27cb827
Adam Jarvis <adam@jarv.ski>
2016-06-28 22:52:04 +0100

    updates to install

--- library/CavTools/install.php
+++ library/CavTools/install.php
@@ -22,8 +22,6 @@ class CavTools_Install {
                 `under_age` TINYINT ( 1 ) NOT NULL,
                 `current_status` TINYINT ( 1 ) NOT NULL,
                 `last_update` BIGINT ( 20 ) NOT NULL,
-                `rtc_thread_id` INT ( 10 ),
-                `s2_thread_id` INT ( 10 ),
                 PRIMARY KEY (`enlistment_id`)
                 )
             ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
@@ -39,7 +37,36 @@ class CavTools_Install {
                 PRIMARY KEY (`log_id`)
                 )
             ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
-        'dropRRDLogs' => 'DROP TABLE IF EXISTS `xf_ct_rrd_logs`'
+        'dropRRDLogs' => 'DROP TABLE IF EXISTS `xf_ct_rrd_logs`',
+
+        'createS3Events' => 'CREATE TABLE IF NOT EXISTS `xf_ct_s3_events` (             
+                `event_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `event_type` INT( 10 ) UNSIGNED NOT NULL,
+                `event_title` VARCHAR( 200 ) NOT NULL ,
+                `event_date` BIGINT ( 20 ) NOT NULL,
+                `event_time` BIGINT ( 20 ) NOT NULL,
+                `event_game` VARCHAR ( 50 ) NOT NULL ,
+                `event_text` LONGTEXT NOT NULL ,
+                `username` VARCHAR ( 50 ) NOT NULL ,
+                `user_id` INT ( 10 ) NOT NULL ,
+                `hidden` TINYINT ( 50 ) NOT NULL ,
+                `thread_id` INT ( 10 ) NOT NULL ,
+                PRIMARY KEY (`event_id`)
+                )
+            ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
+        'dropS3Events' => 'DROP TABLE IF EXISTS `xf_ct_s3_events`',
+
+        'createS3Classes' => 'CREATE TABLE IF NOT EXISTS `xf_ct_s3_classes` (             
+                `class_id` INT( 10 ) UNSIGNED NOT NULL AUTO_INCREMENT,
+                `class_name` VARCHAR ( 50 ) NOT NULL,
+                `class_text` LONGTEXT NOT NULL ,
+                `username` VARCHAR( 50 ) NOT NULL ,
+                `user_id` INT ( 10 ) NOT NULL ,
+                `hidden` TINYINT ( 10) NOT NULL ,
+                PRIMARY KEY (`class_id`)
+                )
+            ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_general_ci;',
+        'dropS3Classes' => 'DROP TABLE IF EXISTS `xf_ct_s3_classes`'
     );
 
     // This is the function to create a table in the database so our addon will work.
@@ -48,6 +75,8 @@ class CavTools_Install {
         $db = XenForo_Application::getDb();
         $db->query(self::$table['createEnlistments']);
         $db->query(self::$table['createRRDLogs']);
+        $db->query(self::$table['createS3Events']);
+        $db->query(self::$table['createS3Classes']);
     }
 
     // This is the function to DELETE the table of our addon in the database.
@@ -56,19 +85,8 @@ class CavTools_Install {
         $db = XenForo_Application::getDb();
         $db->query(self::$table['dropEnlistments']);
         $db->query(self::$table['dropRRDLogs']);
-    }
-
-    protected function _getFields()
-    {
-        return array(
-            'xf_trophy' => array(
-                'trophy_id'   => array('type' => self::TYPE_UINT, 'autoIncrement' => true),
-                'trophy_points' => array('type' => self::TYPE_UINT, 'required' => true),
-                'user_criteria' => array('type' => self::TYPE_UNKNOWN, 'required' => true,
-                    'verification' => array('$this', '_verifyCriteria')
-                )
-            )
-        );
+        $db->query(self::$table['dropS3Events']);
+        $db->query(self::$table['dropS3Classes']);
     }
 }
 
