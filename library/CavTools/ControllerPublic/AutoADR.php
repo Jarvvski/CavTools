ccc5d63bd3ac6c7103c125f6a46a7c4a8b4c6cfe
Adam Jarvis <adam@jarv.ski>
2016-07-01 11:14:57 +0100

    added commets for ADR

--- library/CavTools/ControllerPublic/AutoADR.php
+++ library/CavTools/ControllerPublic/AutoADR.php
@@ -2,46 +2,51 @@
 
 class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstract {
 
+    // Runs on Page load
     public function actionIndex()
     {
-        //Get values from options
+        // Get enable value
         $enable = XenForo_Application::get('options')->enableADR;
 
+        // If not enabled, escape
         if(!$enable) 
         {
             throw $this->getNoPermissionResponseException();
         }
 
+        // If cannot view, escape
         if (!XenForo_Visitor::getInstance()->hasPermission('CavToolsGroupId', 'viewADR'))
         {
             throw $this->getNoPermissionResponseException();
         }
-        
+
+        // If cannot action, don't display the button to rebuild
         if (XenForo_Visitor::getInstance()->hasPermission('CavToolsGroupId', 'canUpdateADR'))
         {
             $canAction = true;
         } else {
             $canAction = false;
         }
-        
+
+        // Get the latest ADR construct
         $model = $this->_getADRModel();
         $data = $model->getLatestADR();
 
 
-        //View Parameters
+        // View Parameters
         $viewParams = array(
             'data' => $data['text'],
             'canAction' => $canAction
         );
 
-        //Send to template to display
+        // Send to template to display
         return $this->responseView('CavTools_ViewPublic_ADR', 'CavTools_adr', $viewParams);
     }
 
+    // Runs on Page POST
     public function actionPost()
     {
-        $visitor  = XenForo_Visitor::getInstance()->toArray();
-        
+        // Create the new ADR then store in DB
         $data = $this->createList();
         $this->createData($data);
 
@@ -52,27 +57,38 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
         );
     }
 
+    // Func to build ADR
     public function createList()
     {
+        // Set initial vars
         $data = "";
         $newLine= "<br />";
+
+        // Get ADR data model
         $model = $this->_getADRModel();
+
+        // Get home URL from enlistment options
         $home = XenForo_Application::get('options')->homeURL;
 
-
+        // Begin members build
         $members = $model->getPrimaryPosIDs();
-        $data .= $newLine;
+
+        // Get all secondary billets
         $secondaryBillets = $model->getSecondaryPosIDs();
 
+        // Begin build of secondary billets
         $extraMembers = array();
         foreach ($secondaryBillets as $secondaryBillet)
         {
+            // All the individual position IDs
             $billets = explode(',', $secondaryBillet['CAST(t1.secondary_position_ids AS CHAR(100))']);
 
-
+            // Iterate through all Pos IDs
             foreach ($billets as $billet) {
+                // Get the data for given Pos ID
                 $positionData = $model->getPositionData($billet);
 
+                // Build a member for that position
                 $secondaryMembers = array(
                     'position_id' => $positionData['position_id'],
                     'position_title' => $positionData['position_title'],
@@ -82,12 +98,17 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
                     'materialized_order' => $positionData['materialized_order'],
                     'position_group_id' => $positionData['position_group_id']
                 );
+
+                // Add this member to secondary billet list
                 array_push($extraMembers ,$secondaryMembers);
             }
         }
+        // Combine secondary billets with primary
         $members = array_merge($members, $extraMembers);
+        // Sort member list via Milpacs display order
         $this->sksort($members,"materialized_order", true);
 
+        // Begin headings
         $hqData = "<br><h3>Regimental Headquarters</h3><hr><br>";
         $bat1HQData = "<br><h3>1-7 Command</h3><hr><br>";
         $suptData = "<br><h3>Support Departments</h3><hr><br>";
@@ -101,7 +122,8 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
         $charlie2Data = "<br><h3>Charlie Company</h3><hr><br>";
         $newRecruitsData = "<br><h3>New Recruits</h3><hr><br>";
         $starterData = "<br><h3>Starter Company</h3><hr><br>";
-        
+
+        // Begin Support headings
         $jagData = "<br><h3>Judge Advocate General's Office (JAG)</h3><hr><br>";
         $s1Data = "<br><h3>S1 Department</h3><hr><br>";
         $s2Data = "<br><h3>S2 Military Intelligence Department</h3><hr><br>";
@@ -112,10 +134,14 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
         $rtcData = "<br><h3>Recruit Training Command (RTC)</h3><hr><br>";
         $ncoData = "<br><h3>NCO Academy</h3><hr><br>";
         $s6Data = "<br><h3>S6 - Regimental IMO (Information Management Office)</h3><hr><br>";
-        
+
+        // Create data entry for each member
         foreach ($members as $member) {
+            // Build Profile link
             $userURL = '<a href="/members/'.$member['user_id'].'">'.$member['title']." ".$member['username'].'</a>';
 
+            // Decide which data group to assign
+            // TODO - change from magic numbers to Xen options
             switch ($member['position_group_id'])
             {
                 case '1':
@@ -125,6 +151,8 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
                     $bat1HQData .= "<p>".$member['position_title'] . " : " . $userURL . $newLine."</p>";
                     break;
                 case '4':
+                    // All support department
+                    // Decide on which department via position ID
                     switch($member['position_id'])
                     {
                         case ($member['position_id'] == '15' || $member['position_id'] == '16' || $member['position_id'] == '17' || $member['position_id'] == '219'):
@@ -192,21 +220,27 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
             }
         }
 
+        // Build Battalion Data
         $bat1Total = "<h3>1st Battalion</h3><hr><br>" . $bat1HQData . $alpha1Data . $bravo1Data . $charlie1Data . $training1Data;
         $bat2Total = "<h3>2nd Battalion</h3><hr><br>" . $bat2HQData . $alpha2Data . $bravo2Data . $charlie2Data;
 
+        // Build Support Data
         $supportTotal = $jagData . $newLine . $s1Data . $newLine . $s2Data . $newLine .
             $mpData . $newLine . $s3Data . $newLine . $s5Data . $newLine .
             $rrdData . $newLine .$rtcData . $newLine .$ncoData . $newLine .
             $s6Data  . $newLine;
-        
-        $data .= $hqData . $supportTotal . $bat1Total .$newLine . $newLine. $bat2Total .
 
+        // Combine data sets
+        $data .= $newLine . $hqData . $supportTotal . $bat1Total .$newLine . $newLine. $bat2Total;
+
+        // Give our data some breathing space
         $data .= $newLine. $newLine . $newLine;
 
+        // Return new ADR
         return $data;
     }
 
+    // Sorting fun
     public function sksort(&$array, $subkey="id", $sort_ascending=false) {
 
         if (count($array))
@@ -234,7 +268,8 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
 
         else $array = $temp_array;
     }
-    
+
+    // Calls DataWriter
     public function createData($data)
     {
         $dw = XenForo_DataWriter::create('CavTools_DataWriter_ADR');
@@ -242,6 +277,7 @@ class CavTools_ControllerPublic_AutoADR extends XenForo_ControllerPublic_Abstrac
         $dw->save();
     }
 
+    // Gets ADR data model
     protected function _getADRModel()
     {
         return $this->getModelFromCache ( 'CavTools_Model_ADR' );
