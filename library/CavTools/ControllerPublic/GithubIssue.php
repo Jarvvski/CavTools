96b09ca130dd211bb8eca05f769a21b85cfa1e48
Adam Jarvis <adam@jarv.ski>
2016-07-03 06:49:31 +0100

    cleaned API call

--- library/CavTools/ControllerPublic/GithubIssue.php
+++ library/CavTools/ControllerPublic/GithubIssue.php
@@ -62,6 +62,31 @@ class CavTools_ControllerPublic_GithubIssue extends XenForo_ControllerPublic_Abs
         //Action can only be called via post
         $this->_assertPostOnly();
         // done
+
+        $title    = $this->_input->filterSingle('title', XenForo_Input::STRING);
+        $problem  = $this->_input->filterSingle('problem', XenForo_Input::STRING);
+        $reason   = $this->_input->filterSingle('reason', XenForo_Input::STRING);
+        
+        $content = array(
+                        'title' => $title,
+                        'problem' => $problem,
+                        'reason' => $reason
+                        );
+        
+        $this->callAPI($content);
+
+        // redirect back to the normal scratchpad index page
+        return $this->responseRedirect(
+            XenForo_ControllerResponse_Redirect::SUCCESS,
+            XenForo_Link::buildPublicLink('forums'),
+            new XenForo_Phrase('Request sent')
+        );
+    }
+
+    public function callAPI($content) {
+
+        // curl -H 'Authorization: token $token' -d '{"title": "My title", "body":"My content"}' https://api.github.com/repos/$repoOwner/$repo/issues
+
         //Get values from options
         $githubToken  = XenForo_Application::get('options')->githubToken;
         $githubRepo      = XenForo_Application::get('options')->githubRepo;
@@ -75,29 +100,12 @@ class CavTools_ControllerPublic_GithubIssue extends XenForo_ControllerPublic_Abs
         $label = $githubIssueLabel;
         $username = $this->getUsername();
         $rank     = $this->getRank();
-        $title    = $this->_input->filterSingle('title', XenForo_Input::STRING);
-        $problem  = $this->_input->filterSingle('problem', XenForo_Input::STRING);
-        $reason   = $this->_input->filterSingle('reason', XenForo_Input::STRING);
-
-        $this->callAPI($repoOwner, $repo, $token, $title, $problem, $reason, $username, $rank, $label);
-
-        // redirect back to the normal scratchpad index page
-        return $this->responseRedirect(
-            XenForo_ControllerResponse_Redirect::SUCCESS,
-            XenForo_Link::buildPublicLink('forums'),
-            new XenForo_Phrase('request_received')
-        );
-    }
-
-    public function callAPI($repoOwner, $repo, $token, $formTitle, $problem, $reason, $username, $rank, $label) {
-
-        // curl -H 'Authorization: token $token' -d '{"title": "My title", "body":"My content"}' https://api.github.com/repos/$repoOwner/$repo/issues
-
+        
         //Set variables
         $headerValue = " Bearer " . $token;
         $header = array("Authorization:" . $headerValue);
-        $title = $formTitle;
-        $body  = sprintf("<h2>Problem</h2><br />%s<br /><hr><h2>Reason</h2><br />%s<br><br>-%s %s", $problem, $reason, $rank, $username);
+        $title = $content['title'];
+        $body  = sprintf("<h2>Problem</h2><br />%s<br /><hr><h2>Reason</h2><br />%s<br><br>-%s %s", $content['problem'], $content['reason'], $rank, $username);
         $data = array("title" => $title, "body" => $body, "labels" => array($label));
         $data_string = json_encode($data);
         $url   = sprintf("https://api.github.com/repos/%s/%s/issues", $repoOwner, $repo);
